8fa5bc2123c1949cf48ee4d82746f7c39c629518
tim-khusus <donyadeachmadi@gmail.com>
2026-03-25 17:34:07 +0700

    Update fundamental.js

--- api/fundamental.js
+++ api/fundamental.js
@@ -2,7 +2,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
+
+
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { ticker } = req.query;
